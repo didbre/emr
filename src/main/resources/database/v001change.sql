f5ca0d1d06a53c97332bd56d563608e09cc24f43
Didier Breton <didier.breton@gmail.com>
2020-04-17 12:38:14 -0400

    add insert

--- src/main/resources/database/v001change.sql
+++ src/main/resources/database/v001change.sql
@@ -23,3 +23,8 @@ create table "patient"
 
     primary key (id)
 );
+
+INSERT INTO patient (id, address_line_1, address_line_2, address_line_3, app_or_local, birth_date, city, country, email,
+                     first_name, hin, last_name, middle_name, phone_extension, phone_number, province, zip_code)
+VALUES (0, 'test', 'test2', 'test3', 'test4', '2017-03-14', 'test6', 'test7', 'test8', 'test9', 'test10', 'test11',
+        'test12', 'test13', 'test14', 'test15', 'test16')
