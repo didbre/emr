ff645cbc2540d6a8072bac44a560e864ffb760d8
didbre <didier.breton@gmail.com>
2020-04-23 07:29:25 -0400

    Continuing implementation of update/1create and validation

--- src/main/java/com/didbre/emr/service/validator/PatientValidator.java
+++ src/main/java/com/didbre/emr/service/validator/PatientValidator.java
@@ -1,57 +1,62 @@
 package com.didbre.emr.service.validator;
 
+import com.didbre.emr.domain.Patient;
 import com.didbre.emr.repository.PatientRepository;
 import com.didbre.emr.service.vo.PatientVO;
+import org.apache.commons.beanutils.BeanUtils;
 
-/**
- * Validation for Patient before interacting with the database
- */
-public class PatientValidator
-{
-    private final PatientRepository patientRepository;
+import java.util.NoSuchElementException;
 
-    public PatientValidator(PatientRepository patientRepository)
-    {
-        this.patientRepository = patientRepository;
+/** Validation for Patient before interacting with the database */
+public class PatientValidator {
+  private final PatientRepository patientRepository;
+
+  public PatientValidator(PatientRepository patientRepository) {
+    this.patientRepository = patientRepository;
+  }
+
+  /**
+   * Validate Patient before save
+   *
+   * @param patientVO
+   * @throws Exception
+   */
+  protected void validateSave(PatientVO patientVO) throws Exception {
+    //        check if the id is null
+    if (patientVO.getId() != null) {
+      //            throw error. Save patient cannot have id. Generate id done at db level
+    }
+  }
+
+  /**
+   * Validate Patient before update
+   *
+   * @param patientVO
+   * @throws Exception
+   */
+  protected PatientVO validateUpdate(PatientVO patientVO) throws Exception {
+    //        check if patient exist
+    if (patientVO.getId() == null) {
+      //            patient id cannot be null
+      //            todo change exception
+      throw new Exception("Cannot update a patient without id");
+    }
+    try {
+      patientRepository.findById(patientVO.getId());
+    } catch (Exception e) {
+      //            patient is not in the database
+      throw new NoSuchElementException(
+          "Patient with ID <" + patientVO.getId() + "> is not existing");
     }
 
-    /**
-     * Validate Patient before save
-     *
-     * @param patientVO
-     * @throws Exception
-     */
-    protected void validateSave(PatientVO patientVO) throws Exception
-    {
-//        check if the id is null
-        if (patientVO.getId() != null)
-        {
-//            throw error. Save patient cannot have id. Generate id done at db level
-        }
-    }
+    Patient patient = new Patient();
+    BeanUtils.copyProperties(patient, patientVO);
+    Patient save = patientRepository.save(patient);
+    PatientVO patientVO1 = new PatientVO();
+    BeanUtils.copyProperties(patientVO1, save);
 
-    /**
-     * Validate Patient before update
-     * @param patientVO
-     * @throws Exception
-     */
-    protected void validateUpdate(PatientVO patientVO) throws Exception
-    {
-//        check if patient exist
-        if (patientVO.getId() == null)
-        {
-//            patient id cannot be null
-//            todo change exception
-            throw new Exception("Cannot update a patient without id");
-        }
-        try
-        {
-            patientRepository.findById(patientVO.getId());
-        }
-        catch (Exception e)
-        {
-//            patient is not in the database
+    return patientVO1;
 
-        }
-    }
+
+  }
 }
